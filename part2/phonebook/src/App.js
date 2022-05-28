ed44c41544d76d2a9af0db5fab7203a4ddb22088
Sauli Kokkonen <sauli.kokkonen@aalto.fi>
2022-05-28 21:36:06 +0300

    Init phonebook and finish step 5

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,65 @@
+import { useState } from 'react'
+
+const InputField = ({ desc, value, action }) => 
+  <div>{desc}: <input value={value} onChange={action} /></div>
+
+const PersonForm = ({ action, name, number, setName, setNumber }) => 
+  <form onSubmit={action}>
+    <InputField desc="Name" value={name} action={setName} />
+    <InputField desc="Number" value={number} action={setNumber} />
+    <button type="submit">add</button>
+  </form>
+
+const Persons = ({ persons, filter }) => 
+  persons
+    .filter(person => person.name.toUpperCase().includes(filter.toUpperCase()))
+    .map(person => <div key={person.name}>{person.name} {person.number}</div>)
+
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filterParams, setFilterParams] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+
+    if (persons.some(person => person.name === newName))
+      return alert(`${newName} is already added to phonebook`)
+
+    setPersons(persons.concat({ name: newName, number: newNumber }))
+    setNewName('')
+    setNewNumber('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <InputField
+        desc={"Filter shown with"}
+        value={filterParams}
+        action={(event => setFilterParams(event.target.value))}
+      />
+
+      <h3>Add a new</h3>
+      <PersonForm
+        action={addPerson}
+        name={newName}
+        setName={(event) => setNewName(event.target.value)}
+        number={newNumber}
+        setNumber={(event) => setNewNumber(event.target.value)}
+      />
+      
+      <h3>Numbers</h3>
+      <Persons persons={persons} filter={filterParams} />
+    </div>
+  )
+}
+
+export default App
