be2fda5d632b93fc2f09a9d43c12ee565ba31046
Sauli Kokkonen <sauli.kokkonen@aalto.fi>
2022-05-31 20:27:25 +0300

    Init countries

---
+++ part2/countries/src/App.js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+function App() {
+  // const hook = () => {
+  //   axios.get('http://localhost:3001/persons').then(response => {
+  //     setPersons(response.data)
+  //   })
+  // }
+
+  // useEffect(hook, [])
+
+  return <p>Hello World!</p>
+}
+
+export default App
