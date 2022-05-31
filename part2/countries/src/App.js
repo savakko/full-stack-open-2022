4733c5a4ffaacbb844503101b6e1b3435074ebc9
savakko <sale.kokkonen@gmail.com>
2022-05-31 22:31:31 +0300

    Finish step 2 of countries

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,16 +1,56 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 
-function App() {
-  // const hook = () => {
-  //   axios.get('http://localhost:3001/persons').then(response => {
-  //     setPersons(response.data)
-  //   })
-  // }
+const CountryList = ({ countries, searchParams }) => {
+  const list = countries.filter(country =>
+    country.name.common.toUpperCase().includes(searchParams.toUpperCase()))
 
-  // useEffect(hook, [])
+  if (list.length > 10) {
+    return <div>Too many matches, specify another filter</div>
+  }
+  if (list.length === 1) {
+    return <Country country={list[0]} />
+  }
 
-  return <p>Hello World!</p>
+  return (
+    <div>
+      {list.map(country => 
+        <div key={country.name.official}>{country.name.official}</div>)}
+    </div>
+  )
+}
+const Country = ({ country }) => 
+  <div>
+    <h1>{country.name.common}</h1>
+    <div>
+      capital {country.capital}<br />
+      area {country.area}
+    </div>
+    <h3>languages:</h3>
+    <ul>
+      {Object.values(country.languages)
+        .map(language => <li key={language}>{language}</li>)}
+    </ul>
+    <img src={country.flags.png} alt="No flag available"></img>
+  </div>
+
+
+const App = () => {
+  const [countries, setCountries] = useState([])
+  const [searchParams, setSearchParams] = useState('')
+
+  useEffect(() => {
+    axios.get('https://restcountries.com/v3.1/all').then(response => {
+      setCountries(response.data)
+    })
+  }, [])
+
+  return (
+    <div>
+      find countries <input value={searchParams} onChange={(event) => setSearchParams(event.target.value)} />
+      <CountryList countries={countries} searchParams={searchParams} />
+    </div>
+  )
 }
 
 export default App
