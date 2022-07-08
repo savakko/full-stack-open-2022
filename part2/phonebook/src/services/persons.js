090103c233c19d43b40072514f325c95209fa643
Sauli Kokkonen <sale.kokkonen@gmail.com>
2022-07-08 14:08:36 +0300

    Move backend endpoints into a basepath variable

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,19 +1,21 @@
 import axios from 'axios'
 
+const basepath = 'http://localhost:3001/api/persons'
+
 const getPersons = async () => {
-  return axios.get('http://localhost:3001/persons').then(response => response.data)
+  return axios.get(basepath).then(response => response.data)
 }
 
 const postPerson = async (person) => {
-  return axios.post('http://localhost:3001/persons', person).then(response => response.data)
+  return axios.post(basepath, person).then(response => response.data)
 }
 
 const updatePerson = async (person) => {
-  return axios.put(`http://localhost:3001/persons/${person.id}`, person).then(response => response.data)
+  return axios.put(`${basepath}/${person.id}`, person).then(response => response.data)
 }
 
 const deletePerson = async (person) => {
-  return axios.delete(`http://localhost:3001/persons/${person.id}`).then(response => response.data)
+  return axios.delete(`${basepath}/${person.id}`).then(response => response.data)
 }
 
 const db = { getPersons, postPerson, updatePerson, deletePerson }
