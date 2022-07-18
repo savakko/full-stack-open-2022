f7b84ae23579b43985c876f71d2cb63c4546efb1
Sauli Kokkonen <sale.kokkonen@gmail.com>
2022-07-18 13:52:55 +0300

    Modularize initial application structure

---
+++ part4/bloglist/app.js
@@ -0,0 +1,20 @@
+const cors = require('cors')
+const express = require('express')
+const app = express()
+const blogRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+const mongoose = require('mongoose')
+
+logger.info('connecting to database...')
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => logger.info('database connection successful'))
+  .catch(error => logger.error('database connection failed:', error.message))
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogRouter)
+
+module.exports = app
