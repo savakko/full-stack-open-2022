18618e77a3cdb418856075ac091f6b13e738ed52
Sauli Kokkonen <sale.kokkonen@gmail.com>
2022-07-19 10:07:07 +0300

    Add error handling middleware

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -2,6 +2,7 @@ const cors = require('cors')
 const express = require('express')
 const app = express()
 const blogRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
 const mongoose = require('mongoose')
@@ -16,5 +17,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogRouter)
+
+app.use(middleware.errorHandler)
 
 module.exports = app
