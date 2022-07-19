60d631744b48ec8054b9e155e7bff1bee1aabd85
Sauli Kokkonen <sale.kokkonen@gmail.com>
2022-07-19 11:07:18 +0300

    Refactor blogRouter to use async methods

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,36 +1,28 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogRouter.get('/', (request, response, next) => {
-  Blog
-    .find({})
-    .then(blogs => response.json(blogs))
-    .catch(error => next(error))
+blogRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
-blogRouter.post('/', (request, response, next) => {
+blogRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
-  
-  blog
-    .save()
-    .then(result => response.status(201).json(result))
-    .catch(error => next(error))
+
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog)
 })
 
-blogRouter.get('/:id', (request, response, next) => {
-  Blog.findById(request.params.id)
-    .then(blog => {
-      if (!blog)
-        return response.status(404).end()
-      response.json(blog)
-    })
-    .catch(error => next(error))
+blogRouter.get('/:id', async (request, response) => {
+  const blog = await Blog.findById(request.params.id)
+  if (!blog)
+    return response.status(404).end()
+  response.json(blog)
 })
 
-blogRouter.delete('/:id', (request, response, next) => {
-  Blog.findByIdAndDelete(request.params.id)
-    .then(() => response.status(204).end())
-    .catch(error => next(error))
+blogRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndDelete(request.params.id)
+  response.status(204).end()
 })
 
 module.exports = blogRouter
