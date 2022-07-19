f02e41491d878e7baa6b790dc3706dd5d9266a9d
Sauli Kokkonen <sale.kokkonen@gmail.com>
2022-07-19 10:50:10 +0300

    Add get and delete blogs by id endpoints

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -17,4 +17,20 @@ blogRouter.post('/', (request, response, next) => {
     .catch(error => next(error))
 })
 
+blogRouter.get('/:id', (request, response, next) => {
+  Blog.findById(request.params.id)
+    .then(blog => {
+      if (!blog)
+        return response.status(404).end()
+      response.json(blog)
+    })
+    .catch(error => next(error))
+})
+
+blogRouter.delete('/:id', (request, response, next) => {
+  Blog.findByIdAndDelete(request.params.id)
+    .then(() => response.status(204).end())
+    .catch(error => next(error))
+})
+
 module.exports = blogRouter
