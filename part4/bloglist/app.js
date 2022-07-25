f94c1a896e16ef81f0e1ea362216714364140d73
Sauli Kokkonen <sale.kokkonen@gmail.com>
2022-07-25 22:40:05 +0300

    Add user management with its testing

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 require('express-async-errors')
 const app = express()
 const blogRouter = require('./controllers/blogs')
+const userRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
@@ -18,6 +19,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogRouter)
+app.use('/api/users', userRouter)
 
 app.use(middleware.errorHandler)
 
