c18ee03f8eb0f03018c85d5f920fc5f577aff6b4
Sauli Kokkonen <sale.kokkonen@gmail.com>
2022-07-26 18:44:26 +0300

    Add token authorization

--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -20,6 +20,10 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).send({ error: 'malformatted id' })
   if (error.name === 'ValidationError')
     return response.status(400).json({ error: error.message })
+  if (error.name === 'JsonWebTokenError')
+    return response.status(401).json({ error: 'invalid token' })
+  if (error.name === 'TokenExpiredError')
+    return response.status(401).json({ error: 'token expired' })
 
   next(error)
 }
