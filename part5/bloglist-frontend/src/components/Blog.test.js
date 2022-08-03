5666be82bea09987c830c33d7b6b477e29fc657e
Sauli Kokkonen <sale.kokkonen@gmail.com>
2022-08-03 13:48:23 +0300

    Expand blog unit tests

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -4,7 +4,7 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('a blog is rendered', () => {
+describe('<Blog />', () => {
   const blog = {
     title: 'Esimerkkiblogi',
     author: 'Erkki',
@@ -16,41 +16,47 @@ test('a blog is rendered', () => {
     }
   }
 
-  render(<Blog blog={blog} />)
+  test('is initially rendered only with the title and author', () => {
+    render(<Blog blog={blog} />)
 
-  const element = screen.getByText(`${blog.title} ${blog.author}`)
-  screen.debug(element)
-})
+    const titleElement  = screen.getByText(blog.title, { exact: false })
+    const authorElement = screen.getByText(blog.author, { exact: false })
+    expect(titleElement) .toBeDefined()
+    expect(authorElement).toBeDefined()
+    expect(titleElement) .toEqual(authorElement)
 
-test('clicking like calls the event handler once', async () => {
-  const blog = {
-    title: 'Esimerkkiblogi',
-    author: 'Erkki',
-    url: 'localhost:9999',
-    likes: 2,
-    user: {
-      username: 'erkkijumala',
-      name: 'Erkki Esimerkki'
-    }
-  }
+    const urlElement   = screen.queryByText(blog.url, { exact: false })
+    const likesElement = screen.queryByText('likes', { exact: false })
+    expect(urlElement).toBeNull()
+    expect(likesElement).toBeNull()
+  })
 
-  const mockHandler = jest.fn()
+  test('renders the url and like count when the view-button is pressed', async () => {
+    render(<Blog blog={blog} />)
 
-  render(
-    <Blog
-      blog={blog}
-      handleLike={mockHandler}
-      handleDelete={mockHandler}
-      owned={true}
-    />
-  )
+    const user = userEvent.setup()
+    const viewButton = screen.getByText('view')
+    await user.click(viewButton)
 
-  const user = userEvent.setup()
-  const viewButton = screen.getByText('view')
-  await user.click(viewButton)
+    const urlElement   = screen.getByText(blog.url, { exact: false })
+    const likesElement = screen.getByText('likes', { exact: false })
+    expect(urlElement)  .toBeDefined()
+    expect(likesElement).toBeDefined()
+  })
 
-  const likeButton = screen.getByText('like')
-  await user.click(likeButton)
+  test('handles the like-event correctly', async () => {
+    const mockHandler = jest.fn()
 
-  expect(mockHandler.mock.calls).toHaveLength(1)
+    render(<Blog blog={blog} handleLike={mockHandler} />)
+
+    const user = userEvent.setup()
+    const viewButton = screen.getByText('view')
+    await user.click(viewButton)
+
+    const likeButton = screen.getByText('like')
+    await user.click(likeButton)
+    await user.click(likeButton)
+
+    expect(mockHandler.mock.calls).toHaveLength(2)
+  })
 })
