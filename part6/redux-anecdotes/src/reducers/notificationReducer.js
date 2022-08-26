442becd2ae9c211c87af3292eae9d619d654d2ee
Sauli Kokkonen <sale.kokkonen@gmail.com>
2022-08-26 15:11:31 +0300

    Implement unique notification timeouts

--- part6/redux-anecdotes/src/reducers/notificationReducer.js
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -1,5 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit"
 
+// This could be modularized to some timeOut helper utility --->
+let notificationId = undefined
+
+const clearNotificationTimeout = () => {
+  clearTimeout(notificationId)
+  notificationId = undefined
+}
+
+const setUniqueTimeout = (operation, duration) => {
+  if (notificationId)
+    clearNotificationTimeout()
+
+  notificationId = setTimeout(() => {
+    operation()
+    clearNotificationTimeout()
+  }, duration)
+}
+// <---
+
 const initialState = ''
 
 const notificationSlice = createSlice({
@@ -20,7 +39,7 @@ export const { setNotification, resetNotification } = notificationSlice.actions
 export const notify = (message, duration = 5000) => {
   return async dispatch => {
     dispatch(setNotification(message))
-    setTimeout(() => dispatch(resetNotification()), duration)
+    setUniqueTimeout(() => dispatch(resetNotification()), duration)
   }
 }
 
